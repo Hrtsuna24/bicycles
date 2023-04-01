20a3aa0df60a6be9ae6ccf97683d504591641251
Hrtsuna24 <93074808+Hrtsuna24@users.noreply.github.com>
2023-04-02 00:29:31 +0300

    2 link list

--- garbagecollections/src/main.cpp
+++ garbagecollections/src/main.cpp
@@ -1,17 +1,25 @@
 #include "Preproc.h"
 #include <string>
 using std::string;
-//#include "Vector.h"
-//using HTM::Vector;
+#include <list>
 
-#include "Set.h"
-#include "Set.cpp"
-using HTM::Set;
+#include "DoubleLinkedList.h"
+#include "DoubleLinkedList.cpp"
+using HTM::DoubleLinkedList;
 
-
+//#include "Set.h"
+//#include "Set.cpp"
+//using HTM::Set;
 
 int main()
 {
-	Set<int> TestMap;
+	
+	DoubleLinkedList<float> list;
+	list.InsertBack(4.f);
+	list.InsertBack(4.f);
+	list.InsertBack(5.f);
+	list.InsertBack(4.f);
+	;
+	list.Delete(list.Find(5.f));
 	return 0;
 }
